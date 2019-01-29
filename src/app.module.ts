7d4c10b30f9c97180e8f85709c900dfb4d1d4e8b
Fabius DE GIOVANNI <fdegi@natsystem.fr>
2019-01-29 16:45:21 +0100

    g module blog

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [BlogModule],
   controllers: [],
   providers: [],
 })
